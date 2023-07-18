4c9b3336fa24c96b51b9ec2dead78aa455269b97
salihcelebi96 <celebisalih277@gmail.com>
2023-07-18 17:43:26 +0300

    7.commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Store from "./pages/StoragePage";
+import Download from "./pages/download";
 
 import './App.css';
 
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
+        <Route path="/store" element={< Download />} />
       </Routes>
     </Router>
   );
